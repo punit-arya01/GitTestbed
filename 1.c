c67874f3222611e5d7d6754f4337b25e0c016c1b
Punit Arya <arya.punit@gmail.com>
2024-08-02 19:58:45 +0530

    Resolved merge conflicts with testing.

--- 1.c
+++ 1.c
@@ -1,1 +1,8 @@
 890
+testing 1.c
+<<<<<<< HEAD
+This is in master branch.
+=======
+This is testing2 branch.
+>>>>>>> testing2
+adding more changed to master's 1.c
--- 2.c
+++ 2.c
@@ -1,3 +1,7 @@
 hello
 print()
 printf()
+void f1()
+print()
+printf()
+one more change to get one more commit to see branch structure
